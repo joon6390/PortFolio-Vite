7c7ef9ac04230bff15d89da142d64c2749db4f29
joon6390 <haejunbag131@google.com>
2025-10-16 12:54:43 +0900

    마무리 완료

--- src/assets/js/smooth.js
+++ src/assets/js/smooth.js
@@ -1,3 +1,13 @@
+import Lenis from 'lenis'
+
 export function smooth() {
-    console.log(smooth)
+    const lenis = new Lenis({
+    autoRaf: true,
+    duration: 1,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    });
+
+    lenis.on('scroll', (e) => {
+        console.log(e);
+    });
 }
