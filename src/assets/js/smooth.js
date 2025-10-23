a74ddd81176ab92e01f880a0109f8cda45bfd5f6
joon6390 <joon6390@naver.com>
2025-10-23 14:33:07 +0900

    수정 사항 반영

--- src/assets/js/smooth.js
+++ src/assets/js/smooth.js
@@ -1,13 +1,21 @@
-import Lenis from 'lenis'
+import Lenis from "@studio-freight/lenis";
 
-export function smooth() {
+const lenis = () => {
     const lenis = new Lenis({
-    autoRaf: true,
-    duration: 1,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+        duration: 1,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     });
 
-    lenis.on('scroll', (e) => {
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    lenis.on("scroll", (e) => {
         console.log(e);
     });
-}
+};
+
+export default lenis;
